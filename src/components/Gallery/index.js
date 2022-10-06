96ba4761f99641de3e002891af8a1854c2bb034e
Roger Retita <roger.retita@gmail.com>
2022-10-06 16:29:05 +0200

    responsive homepage

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,19 @@
-import React/*, {useEffect, useState}*/ from 'react';
-import './Gallery.css';
-import { Link } from 'react-router-dom'
+import React /*, {useEffect, useState}*/ from "react"
+import "./styles.modules.css"
+import { Link } from "react-router-dom"
 
 // import logements from '../data/logements.json'
 
 // async function getLogement() {
-    
+
 //   let response = await fetch('/logements.json');
 //   let dataLogement = await response.json();
-  
+
 //   return dataLogement;
- 
+
 // }
 
-function Gallery (props){
-  
+function Gallery(props) {
   // const [logements, setLogements] = useState([])
 
   // useEffect(()=>{
@@ -24,25 +23,26 @@ function Gallery (props){
   // }, [])
 
   return (
-     <div className='gallery'>
-      {
-        props.logements.map(
-          logement => {
-            return(
-              <Link to={`/logement/${logement.id}`} className='cards' key={ logement.id }>
-                <img src={ logement.cover } className='cards-img' alt={ logement.title }/>
-                <div className='box'></div>
-                <p className='cards-text'>
-                  { logement.title}
-                </p>
-              </Link>
-            )
-          })
-      }
-     </div>
-  );
+    <div className="gallery">
+      {props.logements.map((logement) => {
+        return (
+          <Link
+            to={`/logement/${logement.id}`}
+            className="cards"
+            key={logement.id}
+          >
+            <img
+              src={logement.cover}
+              className="cards-img"
+              alt={logement.title}
+            />
+            <div className="box"></div>
+            <p className="cards-text">{logement.title}</p>
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
 
-export default Gallery ;
-
-
+export default Gallery
