65b500331bd0b461c996ae01ff471b006795ef2a
Roger Retita <roger.retita@gmail.com>
2022-10-06 17:25:00 +0200

    page d'erreur

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './styles.css';
 
 function ErrorPage() {
     return(
         <div className="boxeErrorPage">
             <h2 className="errorNumber">404</h2>
             <p className="errorMessage">Oups ! La page que vous demandez n'existe pas.</p>
+            <Link to={"/"} className="link-to-home">Retourner sur la page d’accueil</Link>
         </div>
     )
 }
